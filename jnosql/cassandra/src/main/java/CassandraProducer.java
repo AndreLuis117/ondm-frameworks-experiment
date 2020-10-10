b9b402ccee27a631a0203412152a54156ec27907
AndreLuis117 <andreluis2006@outlook.com>
2020-10-09 23:14:04 -0300

    Ajustes gerais de chave e valor e colunar

---
+++ jnosql/cassandra/src/main/java/CassandraProducer.java
@@ -0,0 +1,33 @@
+
+
+
+import org.eclipse.jnosql.diana.cassandra.column.CassandraColumnFamilyManager;
+import org.eclipse.jnosql.diana.cassandra.column.CassandraColumnFamilyManagerFactory;
+import org.eclipse.jnosql.diana.cassandra.column.CassandraConfiguration;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+
+@ApplicationScoped
+public class CassandraProducer {
+
+    private static final String KEY_SPACE = "developers";
+
+    private CassandraConfiguration cassandraConfiguration;
+
+    private CassandraColumnFamilyManagerFactory managerFactory;
+
+    @PostConstruct
+    public void init() {
+        cassandraConfiguration = new CassandraConfiguration();
+        managerFactory = cassandraConfiguration.get();
+    }
+
+
+    @Produces
+    public CassandraColumnFamilyManager getManagerCassandra() {
+        return managerFactory.get(KEY_SPACE);
+    }
+
+}
